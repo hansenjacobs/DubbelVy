e02f8b64689b2072b512a3a9f8f8dd1d0821a0d8
Jacob Hansen <hansenjacobs@gmail.com>
2018-07-03 12:11:31 -0500

    corrected audit view to prevent editing is specific roles

--- DubbelVy/Models/AuditViewModels.cs
+++ DubbelVy/Models/AuditViewModels.cs
@@ -10,29 +10,39 @@ namespace Dubbelvy.Models
     {
         public Guid? Id { get; set; }
 
+        [Display(Name = "Template")]
         public int AuditTemplateId { get; set; }
         public AuditTemplate AuditTemplate { get; set; }
 
+        [Display(Name = "Auditee")]
         public string AuditeeId { get; set; }
         public ApplicationUser Auditee { get; set; }
 
+        [Display(Name = "Supervisor")]
         public string SupervisorId { get; set; }
         public ApplicationUser Supervisor { get; set; }
 
+        [Display(Name = "Auditor")]
         public string AuditorId { get; set; }
         public ApplicationUser Auditor { get; set; }
 
+        [Display(Name = "Work Date/Time")]
         public DateTime WorkDateTime { get; set; }
 
+        [Display(Name = "Work Identifier")]
         public string WorkIdentifier { get; set; }
 
+        [Display(Name = "Audit Date/Time")]
         public DateTime? AuditDateTime { get; set; }
 
+        [Display(Name = "Modified")]
         public DateTime? ModifiedDateTime { get; set; }
 
+        [Display(Name = "Modified by")]
         public string ModifiedById { get; set; }
         public ApplicationUser ModifiedBy { get; set; }
 
+        [Display(Name = "Score")]
         public double? Score { get; set; }
 
         public string ScoreDisplay
